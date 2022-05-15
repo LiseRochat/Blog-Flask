5ffca56f9e463ff6384de0fd1a9c07d771099cdf
LiseRochat <liserochat@live.fr>
2022-05-15 11:39:15 +0200

    create function get_db_post

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,14 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+def get_db_post(post_id):
+    dict_post_id = {"id" : post_id}
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = :id', dict_post_id).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
 
 app = Flask(__name__)
 
@@ -24,3 +32,5 @@ def index():
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+
